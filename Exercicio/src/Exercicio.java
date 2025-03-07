639f36352610af455f5f3fd8e7fa03630f32b7bd
predo505 <pedroesquecido1@gmail.com>
2025-03-07 17:04:29 -0300

    Atualizar o Exercicio.java

--- Exercicio/src/Exercicio.java
+++ Exercicio/src/Exercicio.java
@@ -2,27 +2,35 @@ import java.util.Scanner;
 
 public class Exercicio{
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        //Variáveis
+        int idadeAtual, idadeFuturo;
+        double alturaMetros, alturaCentimetros;
+        
+        //Título
         System.out.println(" ");
         System.out.println("Exercício: Manipulação de Variáveis e Operadores");
         System.out.println(" ");
 
-        Scanner scanner = new Scanner(System.in);
-
+        //Seu nome
         System.out.print("Digite seu nome: ");
         String nome = scanner.nextLine();
 
+        //Sua idade
         System.out.print("Digite sua idade: ");
-        int idadeAtual = scanner.nextInt();
-        int idadeFuturo = 50 - idadeAtual;
+        idadeAtual = scanner.nextInt();
+        idadeFuturo = 50 - idadeAtual;
 
+        //Sua altura
         System.out.print("Digite sua altura em metros: ");
-        double alturaMetros = scanner.nextDouble();
-        double alturaCentimetros = alturaMetros * 100;
+        alturaMetros = scanner.nextDouble();
+        alturaCentimetros = alturaMetros * 100;
         System.out.println(" ");
 
+        //Resposta
         System.out.println("Olá, " + nome + "! Em " + idadeFuturo + " anos, você terá 50 anos. ");
-        System.out.println("Sua altura em centímetros é: " + alturaCentimetros + " cm.");
+        System.out.print("Sua altura em centímetros é: " + alturaCentimetros + " cm.");
         scanner.close();
     }
 }
